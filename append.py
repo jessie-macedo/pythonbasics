8be557a64a146b0303dfdd3c3e21486b308c53f3
= <mjessyka.macedo@gmail.com>
2020-12-09 05:44:59 -0300

    Using while and append

---
+++ append.py
@@ -0,0 +1,11 @@
+#Using append =)
+
+x = 0
+List = []
+while x < 5:
+    element = input("Digite números para a lista: ")
+    List.append(element)
+    x +=1
+print("It´s over!")
+print("Let´s look at our list:")
+print(List)
