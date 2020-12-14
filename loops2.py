bc1ca5315a5798c4e0e5a489bf4b1a53b2b22efc
= <mjessyka.macedo@gmail.com>
2020-12-14 07:03:17 -0300

    More basics!

---
+++ loops2.py
@@ -0,0 +1,12 @@
+fantasy_list = ["dragon", "winged serpent", "basilisc", "siren", "pegasus"]
+
+#for species in fantasy_list:
+#    print(species)
+
+#Using break to not go through the entire loop.
+
+for species in fantasy_list:
+    print(species)
+    if species == "basilisc":
+        print("They have a basilisc!")
+        break
