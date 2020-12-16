42c9c6ae5be4f62aec0b8667d46cf4a747de79ab
= <mjessyka.macedo@gmail.com>
2020-12-16 13:29:20 -0300

    Regex + list comprehension

--- regex.py
+++ regex.py
@@ -4,7 +4,10 @@ list_email_users  = ["jenny@gmail.com", "jonny@gmail.com", "kevin@yahoo.com", "l
 
 #gmail = [email for email in list_email_users if re.match("^gmail^", email)]
 #Still trying to figure out
-pattern = "gmail"
-for email in list_email_users:
-    if re.search(pattern, email) is not None: 
-        print(email)
+#pattern = "gmail"
+#for email in list_email_users:
+#    if re.search(pattern, email) is not None: 
+#        print(email)
+
+list_gmail = [email for email in list_email_users if re.search("gmail", email) is not None]
+print(list_gmail)
