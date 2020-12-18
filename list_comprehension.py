5345a07ff33b4cfb54d42cf82f805a467718da40
= <mjessyka.macedo@gmail.com>
2020-12-18 05:35:52 -0300

    A bit more of random

---
+++ list_comprehension.py
@@ -0,0 +1,4 @@
+list = [10, 20, 30, 40, 50, 99]
+
+new_list = [number*10 + 1 for number in list]
+print(new_list)
---
+++ random_example.py
@@ -0,0 +1,8 @@
+import random
+
+random_list = []
+
+random_list = [random.randint(1,200) for index in range(200)]
+print("Random list: ")
+print(random_list)
+
